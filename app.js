dbad114e606deb172d2b9d2258b8a2b6b6ed9bc4
MaiCodes-exe <maise.b.costa@gmail.com>
2023-06-03 21:30:37 +0100

    adding more quotes

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ window.onload = () => {
       let quote = ["Believe you can and you’re halfway there. - Theodore Roosevelt", "Success is not final, failure is not fatal: it is the courage to continue that counts.” - Winston Churchill", "We cannot help everyone, but everyone can help someone. - Ronald Reagan",
     "The only way to do great work is to love what you do. – Steve Jobs",
     "It does not matter how slowly you go as long as you do not stop. – Confucius",
-    
+    "The only true wisdom is in knowing you know nothing. – Socrates",
+    "In three words I can sum up everything I’ve learned about life: it goes on. – Robert Frost",
+    "I am the master of my fate, I am the captain of my soul. – William Ernest Henley",
+  
+    "",
+    ""
     ];
       
       let quoteIndex = Math.floor(Math.random() * quote.length);
