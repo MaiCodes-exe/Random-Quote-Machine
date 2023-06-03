6cf01637ce9add1f6a28593a048c2cf59646d145
MaiCodes-exe <maise.b.costa@gmail.com>
2023-06-03 21:28:00 +0100

    removing duplicated lines

--- app.js
+++ app.js
@@ -10,10 +10,8 @@ window.onload = () => {
     });
     let generateExcuse = () => {
       let quote = ["Believe you can and you’re halfway there. - Theodore Roosevelt", "Success is not final, failure is not fatal: it is the courage to continue that counts.” - Winston Churchill", "We cannot help everyone, but everyone can help someone. - Ronald Reagan"];
-      // let author = ["ate", "peed", "crushed", "broke"];
       
       let quoteIndex = Math.floor(Math.random() * quote.length);
-      // let authorIndex = Math.floor(Math.random() * author.length);
     
       return (
         quote[quoteIndex]);
