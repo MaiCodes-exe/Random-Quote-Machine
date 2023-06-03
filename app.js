b8fbc422e880dc4605f7f18fcf96f6d67fc8c7c0
MaiCodes-exe <maise.b.costa@gmail.com>
2023-06-03 21:29:11 +0100

    adding new quotes

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ window.onload = () => {
 
     });
     let generateExcuse = () => {
-      let quote = ["Believe you can and you’re halfway there. - Theodore Roosevelt", "Success is not final, failure is not fatal: it is the courage to continue that counts.” - Winston Churchill", "We cannot help everyone, but everyone can help someone. - Ronald Reagan"];
+      let quote = ["Believe you can and you’re halfway there. - Theodore Roosevelt", "Success is not final, failure is not fatal: it is the courage to continue that counts.” - Winston Churchill", "We cannot help everyone, but everyone can help someone. - Ronald Reagan",
+    "The only way to do great work is to love what you do. – Steve Jobs",
+    "It does not matter how slowly you go as long as you do not stop. – Confucius",
+    
+    ];
       
       let quoteIndex = Math.floor(Math.random() * quote.length);
     
